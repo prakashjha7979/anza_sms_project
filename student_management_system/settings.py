727094fcd7e900e5836d937ce063ff62db5cf96a
prakash jha <prakashjhakk.pj@gmail.com>
2022-01-17 23:54:47 +0530

    make it better

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-74i60hr^5p$x52vk9fhwc_i_-(ung*oeeb49nmwp8j#$6wu&ul'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['anzasmsproject99.herokuapp.com','localhost', '127.0.0.1']
 
 MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
