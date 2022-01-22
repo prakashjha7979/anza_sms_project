54bc20730e6f71137cd9fbb041565ee28442ee21
prakash jha <prakashjhakk.pj@gmail.com>
2022-01-22 11:12:24 +0530

    do change in settings

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -180,9 +180,9 @@ DEFAULT_FROM_EMAIL="Student management System <theunstoppablejha@gmail.com>"
 
 #Enable Only Making Project Live on Heroku
 # STATICFILES_STORAGE='django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
-# STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
-# import dj_database_url 
-# prod_db=dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(prod_db)
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+import dj_database_url 
+prod_db=dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
