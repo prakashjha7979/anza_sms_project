aaa96751c2e19479ebe995cbb7922ced68b93316
prakash jha <prakashjhakk.pj@gmail.com>
2022-01-22 16:08:57 +0530

    change in staticfile

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -49,6 +49,7 @@ STATICFILES_DIRS = [
 # Application definit
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
