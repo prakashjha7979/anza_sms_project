731933c63de51f57ac9b4277f2537cae81e76f24
prakash jha <prakashjhakk.pj@gmail.com>
2022-01-22 11:11:10 +0530

    do change in setting

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -34,10 +34,14 @@ ALLOWED_HOSTS = ['anzasmsproject99.herokuapp.com','localhost', '127.0.0.1']
 MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 
-STATIC_URL="/staticfiles/"
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL='/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 # STATIC_URL="/static/"
 # STATIC_ROOT=os.path.join(BASE_DIR,"static")
-STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
+
 
 
 
@@ -58,8 +62,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     #===Enable Only Making Project Live on Heroku==
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -176,9 +180,9 @@ DEFAULT_FROM_EMAIL="Student management System <theunstoppablejha@gmail.com>"
 
 #Enable Only Making Project Live on Heroku
 # STATICFILES_STORAGE='django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
-STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
-import dj_database_url 
-prod_db=dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+# import dj_database_url 
+# prod_db=dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
